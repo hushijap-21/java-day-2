a00082156558d527b3b30d200b358d7efec668da
hushijap-21 <hushijap@gmail.com>
2026-02-07 10:54:32 +0530

    Add files via upload

---
+++ hi1.java
@@ -0,0 +1,25 @@
+public class hi1 {
+    public static void main(String[] args) {
+
+        byte b = 100;                 // 1 byte
+        short s = 10000;              // 2 bytes
+        int i = 100000;               // 4 bytes
+        long l = 1000000000L;         // 8 bytes
+
+        float f = 10.5f;              // 4 bytes
+        double d = 99.99;             // 8 bytes
+
+        char c = 'A';                 // 2 bytes
+        boolean flag = true;          // 1 bit (logical)
+
+        System.out.println("byte: " + b);
+        System.out.println("short: " + s);
+        System.out.println("int: " + i);
+        System.out.println("long: " + l);
+        System.out.println("float: " + f);
+        System.out.println("double: " + d);
+        System.out.println("char: " + c);
+        System.out.println("boolean: " + flag);
+    }
+}
+ 
