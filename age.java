8dbf0f2c50945acc4c31fbe00d4f84edf2e655b0
hushijap-21 <hushijap@gmail.com>
2026-02-07 11:30:45 +0530

    Add files via upload

---
+++ age.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class age{
+    public static void main(String[]args){
+        Scanner ob=new Scanner(System.in);
+        System.out.println("enter your age");
+        int age=ob.nextInt();
+        System.out.println("enter your marks");
+        float marks=ob.nextFloat();
+        System.out.println("age:"+age);
+        System.out.println("marks:"+marks);
+        ob.close();
+    }
+}
+
+
+
+
+
+        
+    
