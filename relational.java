247913cf8f1e3b04994446db832cab682eeaa40c
hushijap-21 <hushijap@gmail.com>
2026-02-07 14:02:23 +0530

    Add files via upload

---
+++ relational.java
@@ -0,0 +1,14 @@
+public class relational {
+    public static void main(String[]args){
+        int a=6;
+        int b=4;
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+        System.out.println(a++);
+        System.out.println(++a);
+        System.out.println(a--);
+    }
+    
+}
