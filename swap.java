bad9f4ec68b508a24158ee877f485412ddab377f
hushijap-21 <hushijap@gmail.com>
2026-02-07 15:13:07 +0530

    Add files via upload

---
+++ swap.java
@@ -0,0 +1,14 @@
+public class swap {
+    public static void main(String[]args){
+        int a=10;
+        int b=60;
+        int temp;
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println("After swapping:");
+        System.out.println("enter a value:"+a);
+        System.out.println("enter b value:"+b);
+        }
+    
+}
